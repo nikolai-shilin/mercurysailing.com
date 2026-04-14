6d20c2b65c4ce90c46c7ebea6727aeaabaad18c3
Nikolai Shilin <nikolai.shilin.by@gmail.com>
2026-04-14 13:07:15 +0400

    feat: add next-intl routing config

---
+++ i18n/routing.ts
@@ -0,0 +1,9 @@
+// i18n/routing.ts
+import { defineRouting } from 'next-intl/routing'
+
+export const routing = defineRouting({
+  locales: ['en', 'ru'] as const,
+  defaultLocale: 'en',
+})
+
+export type Locale = (typeof routing.locales)[number]
