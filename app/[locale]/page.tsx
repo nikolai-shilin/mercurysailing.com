0880181092dee72b9fa41fad48d55052b01de460
Nikolai Shilin <nikolai.shilin.by@gmail.com>
2026-05-06 18:12:58 +0400

    Add banner

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,14 +1,13 @@
 import Image from 'next/image'
 import s from './page.module.css'
+import seasonPlans2026 from './season-plans-2026.png'
 
 export default function Home() {
   return (
     <div className={s.container}>
       <Image
-        src="/images/season-plans-2026.png"
+        src={seasonPlans2026}
         alt="Планы на сезон 2026"
-        width={800}
-        height={400}
         priority
         className={s.image}
       />
